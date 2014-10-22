ff0ea1dc2602078bba48cb709d02ba9b7eda09e8
Erik Hommel <hommel@ee-atwork.nl>
2014-10-22 10:05:25 +0200

    tested locally

--- api/v3/DonorJourneyHistory/Correct.php
+++ api/v3/DonorJourneyHistory/Correct.php
@@ -26,14 +26,13 @@ function civicrm_api3_donor_journey_history_correct($params) {
   }
 
   $contacts = array();
-  $contactsSelect = 'SELECT DISTINCT(contact_id) AS contact_id FROM civicrm_subscription_history '
-    . 'WHERE contact_id = 23825 ORDER BY contact_id';
-  $daoContacts = CRM_Core_DAO::executeQuery($contactsSelect);
+  $selectContacts = 'SELECT DISTINCT(contact_id) AS contact_id FROM civicrm_subscription_history '
+    . ' ORDER BY contact_id';
+  $daoContacts = CRM_Core_DAO::executeQuery($selectContacts);
   while ($daoContacts->fetch()) {
     $contacts[] = $daoContacts->contact_id;
   }
   unset($daoContacts);
-  
   foreach ($contacts as $contactId) {
     processSubscriptionHistory($contactId);
   }
@@ -51,13 +50,16 @@ function processSubscriptionHistory($contactId) {
    * only process if there is a subscription history on 01-01-2014 and found
    * group is part of donor journey
    */
-  $query = 'SELECT group_id FROM civicrm_subscription_history WHERE date BETWEEN %1 AND %2';
-  $params = array(1 => array('2013-10-01 00:00:00', 'String'), 2=> array('2013-10-01 23:59:59', 'String'));
-  $dao = CRM_Core_DAO($query, $params);
-  while ($dao->fetch) {
-    if (ocr_check_group_is_donorgroup($dao->group_id) == TRUE) {
-      removeCurrentGroupHistory($dao->group_id, $contactId);
-      checkOtherDonorGroupHistory($dao->group_id, $contactId);
+  $queryFirstDay = 'SELECT group_id FROM civicrm_subscription_history WHERE contact_id = %1 AND (date >= %2 AND date <= %3)';
+  $paramsFirstDay = array(
+    1 => array($contactId, 'Positive'),
+    2 => array('2013-10-01 00:00:00', 'String'), 
+    3 => array('2013-10-01 23:59:59', 'String'));
+  $daoFirstDay = CRM_Core_DAO::executeQuery($queryFirstDay, $paramsFirstDay);
+  while ($daoFirstDay->fetch()) {
+    if (ocr_check_group_is_donorgroup($daoFirstDay->group_id) == TRUE) {
+      removeCurrentGroupHistory($daoFirstDay->group_id, $contactId);
+      checkOtherDonorGroupHistory($daoFirstDay->group_id, $contactId);
     }
   }
 }
@@ -67,16 +69,18 @@ function processSubscriptionHistory($contactId) {
  * @param int $contactId
  */
 function checkOldSubGroups($contactId) {
-  $contactGroups = civicrm_api3('Groups', 'Get', array('contact_id' => $contactId));
-  foreach ($contactGroups['values'] as $contactGroup) {
+  $queryGroupContactSelect = 'SELECT group_id FROM civicrm_group_contact WHERE contact_id = %1';
+  $paramsGroupContactSelect = array(1 => array($contactId, 'Positive'));
+  $daoGroupContactSelect = CRM_Core_DAO::executeQuery($queryGroupContactSelect, $paramsGroupContactSelect);
+  while ($daoGroupContactSelect->fetch()) {
     /*
      * remove data if group is no longer active
      */
-    $query = 'SELECT is_active FROM civicrm_group WHERE id = %1';
-    $params = array(1 => array($contactGroup['group_id'], 'Positive'));
-    $dao = CRM_Core_DAO::executeQuery($query, $params);
-    if ($dao->fetch()) {
-      removeOldSubGroup($contactId, $contactGroup['group_id'], $dao->is_active);
+    $queryGroup = 'SELECT is_active FROM civicrm_group WHERE id = %1';
+    $paramsGroup = array(1 => array($daoGroupContactSelect->group_id, 'Positive'));
+    $daoGroup = CRM_Core_DAO::executeQuery($queryGroup, $paramsGroup);
+    if ($daoGroup->fetch()) {
+      removeOldSubGroup($contactId, $daoGroupContactSelect->group_id, $daoGroup->is_active);
     }
   }
 }
@@ -90,12 +94,15 @@ function checkOldSubGroups($contactId) {
  */
 function removeOldSubGroup($contactId, $groupId, $isActive) {
   if ($isActive == 0) {
-    if (ocr_check_group_is_donorgroup($groupId) == TRUE) {
-      $query = 'DELETE FROM civicrm_subscription_history WHERE contact_id = %1 AND group_id = %2';
-      $params = array(
+    $subGroups = getSubGroups();
+    if (in_array($groupId, $subGroups)) {
+      $queryOldSub = 'DELETE FROM civicrm_subscription_history WHERE contact_id = %1 AND group_id = %2';
+      $paramsOld = array(
         1 => array($contactId, 'Positive'), 
         2 => array($groupId, 'Positive'));
-      CRM_Core_DAO::executeQuery($query, $params);
+      CRM_Core_DAO::executeQuery($queryOldSub , $paramsOld );
+      $queryOldGroup = 'DELETE FROM civicrm_group_contact WHERE contact_id = %1 AND group_id = %2';
+      CRM_Core_DAO::executeQuery($queryOldGroup, $paramsOld);
     }
   }
 }
@@ -107,12 +114,42 @@ function removeOldSubGroup($contactId, $groupId, $isActive) {
  * @param type $contactId
  */
 function removeCurrentGroupHistory($groupId, $contactId) {
-  $query = 'DELETE FROM civicrm_subscription_history WHERE contact_id = %1 AND group_id = %2 AND date > %3';
-  $params = array(
+  $queryCurrentHist = 'DELETE FROM civicrm_subscription_history WHERE contact_id = %1 '
+    . 'AND group_id = %2 AND date > %3';
+  $paramsCurrentHist = array(
     1 => array($contactId, 'Positive'), 
     2 => array($groupId, 'Positive'),
     3 => array('2013-10-01 23:59:59', 'String'));
-  CRM_Core_DAO::executeQuery($query, $params);  
+  $result = CRM_Core_DAO::executeQuery($queryCurrentHist, $paramsCurrentHist);
+  /*
+   * if something has been deleted, also remove GroupContacts
+   */
+  if ($result->N > 0) {
+    removeGroupContacts($contactId, $groupId);
+  }
+}
+/**
+ * Function to remove GroupContacts if group no longer in history
+ * 
+ * @param int $contactId
+ * @param int $groupId
+ */
+function removeGroupContacts($contactId, $groupId) {
+  /*
+   * remove groupcontacts if group no longer in history
+   */
+  $countHist = 'SELECT COUNT(*) AS countGroup FROM civicrm_subscription_history WHERE group_id = %1';
+  $paramsHist = array(1 => array($groupId, 'Positive'));
+  $daoHist = CRM_Core_DAO::executeQuery($countHist, $paramsHist);
+  if ($daoHist->fetch()) {
+    if ($daoHist->countGroup == 0) {
+      $queryGroupContact = 'DELETE FROM civicrm_group_contact WHERE contact_id = %1 AND group_id = %2';
+      $paramsGroupContact = array(
+        1 => array($contactId, 'Positive'),
+        2 => array($groupId, 'Positive'));
+      CRM_Core_DAO::executeQuery($queryGroupContact, $paramsGroupContact);
+    }
+  }
 }
 /**
  * Function to deal with another donor journey group if there is one
@@ -121,30 +158,34 @@ function removeCurrentGroupHistory($groupId, $contactId) {
  * @param type $contactId
  */
 function checkOtherDonorGroupHistory($groupId, $contactId) {
-  $query = 'SELECT group_id, date FROM civicrm_subscription_history WHERE contact_id = %1 '
+  $queryDonorGroupHist = 'SELECT group_id, date FROM civicrm_subscription_history WHERE contact_id = %1 '
     . 'AND group_id != %2 AND date > %3 AND status = %4 ORDER BY date';
-  $params = array(
+  $paramsDonorGroupHist = array(
     1 => array($contactId, 'Positive'), 
     2 => array($groupId, 'Positive'),
     3 => array('2013-10-01 23:59:59', 'String'),
     4 => array('Added', 'String'));
-  $dao = CRM_Core_DAO::executeQuery($query, $params);
-  while ($dao->fetch()) {
-    if (ocr_check_group_is_donorgroup($dao->group_id) == TRUE) {
-      insertRemove($contactId, $groupId, $dao->date);
+  $daoDonorGroupHist = CRM_Core_DAO::executeQuery($queryDonorGroupHist, $paramsDonorGroupHist);
+  while ($daoDonorGroupHist->fetch()) {
+    if (ocr_check_group_is_donorgroup($daoDonorGroupHist->group_id) == TRUE) {
+      insertRemove($contactId, $groupId, $daoDonorGroupHist->date);
       break;
     }
   }
 }
 function insertRemove($contactId, $groupId, $removeDate) {
-  $query = 'INSERT INTO civicrm_subscription_history (contact_id, group_id, date, method, status) '
+  $queryInsert = 'INSERT INTO civicrm_subscription_history (contact_id, group_id, date, method, status) '
     . 'VALUES(%1, %2, %3, %4, %5';
-  $params = array(
+  $paramsInsert = array(
     1 => array($contactId, 'Positive'),
     2 => array($groupId, 'Positive'),
     3 => array($removeDate, 'String'),
     4 => array('API', 'String'),
     5 => array('Removed', 'String')
   );
-  CRM_Core_DAO::executeQuery($query, $params);
+  CRM_Core_DAO::executeQuery($queryInsert, $paramsInsert);
+}
+function getSubGroups() {
+  return array(6524, 6526, 6528, 6531, 6532, 6533, 6534, 6535, 6536, 6537, 6538, 
+    6539, 6540, 6541, 6542, 6543, 6544, 6545, 6546, 6547, 6548, 6549, 6550, 6554, 6555);
 }
