440c4306709adb997fd33f7256360a6c860054a0
Erik Hommel <hommel@ee-atwork.nl>
2014-11-06 11:50:47 +0100

    fix step1a

--- api/v3/DonorLink/Step1a.php
+++ api/v3/DonorLink/Step1a.php
@@ -14,7 +14,7 @@ function civicrm_api3_donor_link_step1a($params) {
   /*
    * read all removes from subscription history in 2014
    */
-  $querySelect = 'SELECT id, contact_id, group_id FROM civicrm_subscription_history '
+  $querySelect = 'SELECT id, contact_id, group_id, date FROM civicrm_subscription_history '
     . 'WHERE status = %1 AND date >= %2 AND contact_id = 28538';
   $paramsSelect = array(
     1 => array('Removed', 'String'),
@@ -25,7 +25,7 @@ function civicrm_api3_donor_link_step1a($params) {
      * if to be removed (no added for contact/group if it is donorgroup
      */
     if (ocr_check_group_is_donorgroup($daoSelect->group_id) == TRUE &&
-    checkToBeDeleted($daoSelect->contact_id, $daoSelect->group_id) == TRUE) {
+    checkToBeDeleted($daoSelect->contact_id, $daoSelect->group_id, $daoSelect->date) == TRUE) {
       correctdonorgroup2014_add_contact($daoSelect->contact_id);
       $queryDelete = 'DELETE FROM civicrm_subscription_history WHERE id = %1';
       $paramsDelete = array(1 => array($daoSelect->id, 'Positive'));
@@ -36,13 +36,14 @@ function civicrm_api3_donor_link_step1a($params) {
   return civicrm_api3_create_success($returnValues, $params, 'DonorLink', 'Step1a');
 }
 
-function checkToBeDeleted($contactId, $groupId) {
-  $query = 'SELECT COUNT(*) AS count_added WHERE contact_id = %1 AND group_id = %2 AND status = %3 AND date >= %4';
+function checkToBeDeleted($contactId, $groupId, $dateRemoved) {
+  $query = 'SELECT COUNT(*) FROM civicrm_subscription_history AS count_added WHERE '
+    . 'contact_id = %1 AND group_id = %2 AND status = %3 AND date < %4';
   $params = array(
     1 => array($contactId, 'Positive'),
     2 => array($groupId, 'Positive'),
     3 => array('Added', 'String'),
-    4 => array('2014-01-01', 'String'));
+    4 => array($dateRemoved, 'String'));
   $dao = CRM_Core_DAO::executeQuery($query, $params);
   if ($dao->fetch()) {
     if ($dao->count_added > 0) {
