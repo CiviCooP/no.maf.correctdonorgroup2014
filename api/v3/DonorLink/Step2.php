ebdc4d77ff4da8e9c171843b4fed177ef7d199d9
Erik Hommel <hommel@ee-atwork.nl>
2014-11-06 14:59:24 +0100

    fixed for test donor 46214

--- api/v3/DonorLink/Step2.php
+++ api/v3/DonorLink/Step2.php
@@ -13,7 +13,7 @@ function civicrm_api3_donor_link_step2($params) {
   $subgroups = array(6521, 6522, 6523, 6256, 6528, 6547, 6548, 6549, 6550, 6554, 
     6555, 6569, 6570, 6562, 6563, 6564, 6556, 6557, 6558, 6559, 6560, 6561, 6572, 
     6573, 6575, 6577, 6581, 6583, 6565, 6566, 6567, 6568, 6571, 6574, 6576, 6578);
-  $query = 'SELECT * FROM civicrm_subscription_history WHERE status = %1 AND date BETWEEN %2 AND %3 and contact_id = 46294';
+  $query = 'SELECT * FROM civicrm_subscription_history WHERE status = %1 AND date BETWEEN %2 AND %3 and contact_id = 46214';
   $paramsQuery = array(
     1 => array('Removed', 'String'),
     2 => array('2014-10-17 00:00:00', 'String'),
